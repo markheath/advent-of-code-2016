133c6cbee133260b56e7d854d1e8cb5efd827752
Mark Heath <mark.heath@gmail.com>
2016-12-01 13:03:30 +0000

    some code cleanup

--- day 01/day1.fsx
+++ day 01/day1.fsx
@@ -4,7 +4,8 @@ type Turn = Right | Left
 type Instruction = Turn of direction : Turn | Move of int
 type State = { position: int * int; facing: Direction; visited: Set<int*int> }
 
-let input = "L3, R1, L4, L1, L2, R4, L3, L3, R2, R3, L5, R1, R3, L4, L1, L2, R2, R1, L4, L4, R2, L5, R3, R2, R1, L1, L2, R2, R2, L1, L1, R2, R1, L3, L5, R4, L3, R3, R3, L5, L190, L4, R4, R51, L4, R5, R5, R2, L1, L3, R1, R4, L3, R1, R3, L5, L4, R2, R5, R2, L1, L5, L1, L1, R78, L3, R2, L3, R5, L2, R2, R4, L1, L4, R1, R185, R3, L4, L1, L1, L3, R4, L4, L1, R5, L5, L1, R5, L1, R2, L5, L2, R4, R3, L2, R3, R1, L3, L5, L4, R3, L2, L4, L5, L4, R1, L1, R5, L2, R4, R2, R3, L1, L1, L4, L3, R4, L3, L5, R2, L5, L1, L1, R2, R3, L5, L3, L2, L1, L4, R4, R4, L2, R3, R1, L2, R1, L2, L2, R3, R3, L1, R4, L5, L3, R4, R4, R1, L2, L5, L3, R1, R4, L2, R5, R4, R2, L5, L3, R4, R1, L1, R5, L3, R1, R5, L2, R1, L5, L2, R2, L2, L3, R3, R3, R1"
+//let input = "L3, R1, L4, L1, L2, R4, L3, L3, R2, R3, L5, R1, R3, L4, L1, L2, R2, R1, L4, L4, R2, L5, R3, R2, R1, L1, L2, R2, R2, L1, L1, R2, R1, L3, L5, R4, L3, R3, R3, L5, L190, L4, R4, R51, L4, R5, R5, R2, L1, L3, R1, R4, L3, R1, R3, L5, L4, R2, R5, R2, L1, L5, L1, L1, R78, L3, R2, L3, R5, L2, R2, R4, L1, L4, R1, R185, R3, L4, L1, L1, L3, R4, L4, L1, R5, L5, L1, R5, L1, R2, L5, L2, R4, R3, L2, R3, R1, L3, L5, L4, R3, L2, L4, L5, L4, R1, L1, R5, L2, R4, R2, R3, L1, L1, L4, L3, R4, L3, L5, R2, L5, L1, L1, R2, R3, L5, L3, L2, L1, L4, R4, R4, L2, R3, R1, L2, R1, L2, L2, R3, R3, L1, R4, L5, L3, R4, R4, R1, L2, L5, L3, R1, R4, L2, R5, R4, R2, L5, L3, R4, R1, L1, R5, L3, R1, R5, L2, R1, L5, L2, R2, L2, L3, R3, R3, R1"
+let input = System.IO.File.ReadAllText (__SOURCE_DIRECTORY__ + "\\input.txt")
 let parseInstruction (i:string) = 
     let turn = match i.[0] with | 'L' -> Left | 'R' -> Right | _ -> failwith "parseError"
     let blocks = int(i.Substring(1))
@@ -18,20 +19,15 @@ Regex.Matches(input, "[RL]\d+")
     |> Seq.map (fun m -> m.Value)
     |> Seq.toArray
 *)
+let rotate start direction = 
+    let directions = [|North;East;South;West|]
+    let offset = if direction = Left then 3 else 1
+    directions.[(Array.IndexOf (directions,start)+offset) % 4]
 
 let move state instruction =
     match instruction with 
     | Turn direction ->
-        let nowFacing = match state.facing, direction with
-                        | North, Left -> West
-                        | North, Right -> East
-                        | West, Left -> South
-                        | West, Right -> North
-                        | South, Left -> East
-                        | South, Right -> West
-                        | East, Left -> North
-                        | East, Right -> South
-        { state with facing = nowFacing }
+        { state with facing = rotate state.facing direction }
     | Move blocks ->
         let x,y = state.position
         let newPos = match state.facing with
@@ -52,13 +48,13 @@ printfn "Part a: Distance from home is %d" (getDistance endState.position)
 
 let expandInstruction i =
     match i with
-    | Turn direction -> [| Turn direction |]
-    | Move blocks -> [| for n in 1..blocks -> Move 1 |]
+    | Turn direction -> [ Turn direction ]
+    | Move blocks -> [ for n in 1..blocks -> Move 1 ]
 
 let visitedTwice = instructions 
-                    |> Array.collect expandInstruction 
-                    |> Array.scan move startState
-                    |> Array.find (fun f -> f.visited.Contains(f.position))
+                    |> Seq.collect expandInstruction 
+                    |> Seq.scan move startState
+                    |> Seq.find (fun f -> f.visited.Contains(f.position))
                     
 printfn "Part b: Distance from home is %d" (getDistance visitedTwice.position)
 // my answer was 143
