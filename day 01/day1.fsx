7043b6aac2c0746a186d07b0151e2ca035b1525a
Mark Heath <mark.heath@gmail.com>
2016-12-01 17:35:53 +0000

    switch to using regex

--- day 01/day1.fsx
+++ day 01/day1.fsx
@@ -1,15 +1,15 @@
-open System
+open System.Text.RegularExpressions
 type Turn = Right | Left
-type Instruction = Turn of direction : Turn | Move
-type State = { position: int * int; facing: int*int; visited: Set<int*int> }
-let input = System.IO.File.ReadAllText (__SOURCE_DIRECTORY__ + "\\input.txt")
-let parseInstruction (i:string) = seq {
-    yield match i.[0] with | 'L' -> Turn Left | 'R' -> Turn Right | _ -> failwith "parseError"
-    yield! [for n in 1..int(i.Substring(1)) -> Move]
-}
-let instructions = input.Split([|", "|], StringSplitOptions.None) |> Seq.collect parseInstruction
-
+type Instruction = Turn of Turn | Move
+type State = { pos: int*int; facing: int*int; visited: Set<int*int> }
+let startState = { pos = (0,0); facing = (0,-1); visited = Set.empty } 
 let rotate (x,y) = function | Right -> (y*(-1),x) | Left -> (y,x*(-1)) 
+let getDistance (x,y) = abs(x) + abs(y)
+let input = System.IO.File.ReadAllText (__SOURCE_DIRECTORY__ + "\\input.txt")
+let instructions = Regex.Matches(input, "([LR])|(\\d+)") 
+                    |> Seq.cast<Match>
+                    |> Seq.map (fun m -> m.Value)
+                    |> Seq.collect (function | "L" -> [Turn Left] | "R" -> [Turn Right] | n -> [for _ in 1..int(n) -> Move])
 
 let move state instruction =
     match instruction with 
@@ -17,18 +17,14 @@ let move state instruction =
         { state with facing = rotate state.facing direction }
     | Move ->
         let addv (x,y) (i,j) = (x+i,y+j)
-        { state with position = addv state.position state.facing; visited = Set.add state.position state.visited  }
+        { state with pos = addv state.pos state.facing; visited = Set.add state.pos state.visited  }
        
-let startState = { position = (0,0); facing = (0,-1); visited = Set.empty } 
-
 let endState = instructions |> Seq.fold move startState
 
-let getDistance (x,y) = abs(x) + abs(y)
-
-printfn "Part a: Distance from home is %d" (getDistance endState.position) // my answer was 252
+printfn "Part a: Distance from home is %d" (getDistance endState.pos) // my answer was 252
 
 let visitedTwice = instructions 
                     |> Seq.scan move startState
-                    |> Seq.find (fun f -> f.visited.Contains(f.position))
+                    |> Seq.find (fun f -> f.visited.Contains(f.pos))
                     
-printfn "Part b: Distance from home is %d" (getDistance visitedTwice.position) // my answer was 143
+printfn "Part b: Distance from home is %d" (getDistance visitedTwice.pos) // my answer was 143
