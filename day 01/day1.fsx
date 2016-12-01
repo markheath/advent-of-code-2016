0a62f492d8ff513f4b5b2d7a46886e69b6696bcd
Mark Heath <mark.heath@gmail.com>
2016-12-01 14:33:19 +0000

    using vectors for direction

--- day 01/day1.fsx
+++ day 01/day1.fsx
@@ -1,9 +1,7 @@
 open System
-type Direction = North | East | South | West
 type Turn = Right | Left
 type Instruction = Turn of direction : Turn | Move
-type State = { position: int * int; facing: Direction; visited: Set<int*int> }
-
+type State = { position: int * int; facing: int*int; visited: Set<int*int> }
 let input = System.IO.File.ReadAllText (__SOURCE_DIRECTORY__ + "\\input.txt")
 let parseInstruction (i:string) = seq {
     yield match i.[0] with | 'L' -> Turn Left | 'R' -> Turn Right | _ -> failwith "parseError"
@@ -11,25 +9,17 @@ let parseInstruction (i:string) = seq {
 }
 let instructions = input.Split([|", "|], StringSplitOptions.None) |> Seq.collect parseInstruction
 
-let rotate start direction = 
-    let directions = [|North;East;South;West|]
-    let offset = if direction = Left then 3 else 1
-    directions.[(Array.IndexOf (directions,start)+offset) % 4]
+let rotate (x,y) = function | Right -> (y*(-1),x) | Left -> (y,x*(-1)) 
 
 let move state instruction =
     match instruction with 
     | Turn direction ->
         { state with facing = rotate state.facing direction }
     | Move ->
-        let x,y = state.position
-        let newPos = match state.facing with
-                        | North -> (x, y - 1)
-                        | West -> (x - 1, y)
-                        | South -> (x, y + 1)
-                        | East -> (x + 1, y)
-        { state with position = newPos; visited = Set.add state.position state.visited  }
+        let addv (x,y) (i,j) = (x+i,y+j)
+        { state with position = addv state.position state.facing; visited = Set.add state.position state.visited  }
        
-let startState = { position = (0,0); facing = North; visited = Set.empty } 
+let startState = { position = (0,0); facing = (0,-1); visited = Set.empty } 
 
 let endState = instructions |> Seq.fold move startState
 
