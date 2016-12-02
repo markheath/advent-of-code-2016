737fbb05ee054eee4a8250ec861bc1704427c50a
Mark Heath <mark.heath@gmail.com>
2016-12-02 08:17:41 +0000

    refactoring towards part b

--- day 02/day2.fsx
+++ day 02/day2.fsx
@@ -5,7 +5,11 @@ LURDL
 UUUUD"
 let input = System.IO.File.ReadAllText (__SOURCE_DIRECTORY__ + "\\input.txt")
 
-let keypad =  Array2D.init 3 3 (fun x y -> (x*3 + y + 1))
+let keypad = [| "     ";
+                " 123 ";
+                " 456 ";
+                " 789 ";
+                "     " |]
 let instructions = input.Split([|'\n'|], StringSplitOptions.RemoveEmptyEntries)
 
 let followInstruction pos instruction = 
@@ -13,22 +17,25 @@ let followInstruction pos instruction =
     let move = match instruction with | 'U' -> (0, -1) | 'D' -> (0, 1) | 'R' -> (1,0) | 'L' -> (-1,0) | _ -> (0,0)
     let x', y' = addv pos move
     let constrain n l u = max (min n u) l 
-    (constrain x' 0 2), (constrain y' 0 2)
+    (constrain x' 1 3), (constrain y' 1 3)
 
 //followInstruction (1,0) 'U'
-followInstruction (1,2) 'L'
+//followInstruction (1,2) 'L'
 
 let followLine pos line =
     line |> Seq.fold followInstruction pos
 
-let folder (pos,nums) line =
-    let x,y = followLine pos line
-    (x,y), (keypad.[y,x])::nums
-
 //instructions |> Seq.fold folder ((1,1),[])
-let lookup (x,y) = keypad.[y,x]
+let lookup (x,y) = keypad.[y].[x]
 
-instructions |> Seq.scan followLine (1,1) |> Seq.skip 1 |> Seq.map lookup
+// part a - my answer is 44558
+instructions 
+    |> Seq.scan followLine (1,1) 
+    |> Seq.skip 1 
+    |> Seq.map lookup 
+    |> Seq.map string 
+    |> String.Concat 
+    |> printfn "Part a: %s"
 
 
 let startPos = 1,1
