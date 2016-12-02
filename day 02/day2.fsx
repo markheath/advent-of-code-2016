2c71d180f62d7f7397afbe707dad519b8b0b8672
Mark Heath <mark.heath@gmail.com>
2016-12-02 08:23:23 +0000

    completed part b

--- day 02/day2.fsx
+++ day 02/day2.fsx
@@ -10,13 +10,24 @@ let keypadA = [| "     ";
                  " 456 ";
                  " 789 ";
                  "     " |]
+
+let keypadB = [| "       ";
+                 "   1   ";
+                 "  234  ";
+                 " 56789 ";
+                 "  ABC  ";
+                 "   D   ";
+                 "       " |]
+
+let keypad = keypadB // keypadA
+let startPos = (3,3) //  (2,2)          
 let instructions = input.Split([|'\n'|], StringSplitOptions.RemoveEmptyEntries)
 
 let followInstruction pos instruction = 
     let addv (x,y) (i,j) = x+i,y+j
     let move = match instruction with | 'U' -> (0, -1) | 'D' -> (0, 1) | 'R' -> (1,0) | 'L' -> (-1,0) | _ -> (0,0)
     let x', y' = addv pos move
-    if keypadA.[y'].[x'] = ' ' then pos else (x',y')
+    if keypad.[y'].[x'] = ' ' then pos else (x',y')
 
 //followInstruction (1,0) 'U'
 //followInstruction (1,2) 'L'
@@ -25,16 +36,13 @@ let followLine pos line =
     line |> Seq.fold followInstruction pos
 
 //instructions |> Seq.fold folder ((1,1),[])
-let lookup (x,y) = keypadA.[y].[x]
+let lookup (x,y) = keypad.[y].[x]
 
 // part a - my answer is 44558
 instructions 
-    |> Seq.scan followLine (1,1) 
+    |> Seq.scan followLine startPos
     |> Seq.skip 1 
     |> Seq.map lookup 
     |> Seq.map string 
     |> String.Concat 
     |> printfn "Part a: %s"
-
-
-let startPos = 1,1
