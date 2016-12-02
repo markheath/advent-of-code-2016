50d9f06a4fa160ec60707e3fa644cf001ceff719
Mark Heath <mark.heath@gmail.com>
2016-12-02 09:28:51 +0000

    refactoring to a solve function

--- day 02/day2.fsx
+++ day 02/day2.fsx
@@ -1,27 +1,26 @@
-let testInput = [| "ULL  "; "RRDDD"; "LURDL";  "UUUUD" |]
-let input = System.IO.File.ReadAllLines (__SOURCE_DIRECTORY__ + "\\input.txt")
+let solve (keypad:string[]) startPos input =
+    let lookup (x,y) = keypad.[y].[x]
+    let isValid pos = lookup pos <> ' '
+    let followInstruction pos instruction = 
+        let addv (x,y) (i,j) = x+i,y+j
+        let move = match instruction with | 'U' -> (0, -1) | 'D' -> (0, 1) | 'R' -> (1,0) | 'L' -> (-1,0) | _ -> (0,0)
+        let newPos = addv pos move
+        if isValid newPos then newPos else pos
+    let followLine pos line =
+        line |> Seq.fold followInstruction pos
+    input 
+        |> Seq.scan followLine startPos
+        |> Seq.skip 1 
+        |> Seq.map (lookup >> string) 
+        |> System.String.Concat 
+        |> printfn "Code: %s"
 
 let keypadA = [| "     "; " 123 "; " 456 "; " 789 "; "     " |]
 let keypadB = [| "       "; "   1   "; "  234  "; " 56789 "; "  ABC  "; "   D   "; "       " |]
 
-let keypad = keypadB // keypadA
-let startPos = (3,3) //  (2,2)          
+let testInput = [| "ULL  "; "RRDDD"; "LURDL";  "UUUUD" |]
+let input = System.IO.File.ReadAllLines (__SOURCE_DIRECTORY__ + "\\input.txt")
 
-let lookup (x,y) = keypad.[y].[x]
-
-let followInstruction pos instruction = 
-    let addv (x,y) (i,j) = x+i,y+j
-    let move = match instruction with | 'U' -> (0, -1) | 'D' -> (0, 1) | 'R' -> (1,0) | 'L' -> (-1,0) | _ -> (0,0)
-    let newPos = addv pos move
-    if lookup newPos = ' ' then pos else newPos
-
-let followLine pos line =
-    line |> Seq.fold followInstruction pos
-
-// part a - my answer is 44558, part b - my answer is 6BBAD
-input 
-    |> Seq.scan followLine startPos
-    |> Seq.skip 1 
-    |> Seq.map (lookup >> string) 
-    |> System.String.Concat 
-    |> printfn "Part a: %s"
+solve keypadA (2,2) testInput
+solve keypadA (2,2) input // part a - my answer is 44558
+solve keypadB (3,3) input // part b - my answer is 6BBAD
