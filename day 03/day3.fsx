729112504e9a643e07732134f1c91a98a61c275d
Mark Heath <mark.heath@gmail.com>
2016-12-03 15:08:18 +0000

    added a readme and simplified solution further

--- day 03/day3.fsx
+++ day 03/day3.fsx
@@ -1,18 +1,16 @@
 open System.Text.RegularExpressions
-let isPossibleTriangle sides =
-    let sorted = sides |> Array.sort
-    sorted.[0] + sorted.[1] > sorted.[2]
+
+let isTriangle = function |[|a;b;c|] -> a+b>c && a+c>b && b+c>a | _ -> failwith "arg error"
 
 let input = System.IO.File.ReadAllLines (__SOURCE_DIRECTORY__ + "\\input.txt")
 
 let getSides line = 
     Regex.Matches(line, "\d+") |> Seq.cast<Match> |> Seq.map (fun m -> int m.Value) |> Seq.toArray
 
-input |> Seq.map getSides |> Seq.filter isPossibleTriangle |> Seq.length |> printfn "Part a: %d" // my answer = 982
+input |> Seq.map getSides |> Seq.filter isTriangle |> Seq.length |> printfn "Part a: %d" // my answer = 982
 
 let rotate (a:'T[][]) = 
-    let range (n:int) = { 0 .. (n - 1) } 
-    [| for x in range a.[0].Length -> [| for y in range a.Length -> a.[y].[x] |]  |]
+    [| for x in {0 .. a.[0].Length-1} -> [| for y in {0 .. a.Length-1} -> a.[y].[x] |] |]
 
 let sides = input |> Array.map getSides |> Array.chunkBySize 3 |> Seq.collect rotate 
-sides |> Seq.filter isPossibleTriangle |> Seq.length |> printfn "Part b: %d" // my answer = 1826
+sides |> Seq.filter isTriangle |> Seq.length |> printfn "Part b: %d" // my answer = 1826
--- readme.md
+++ readme.md
@@ -1,3 +1,11 @@
 ## Advent of Code 2016 Solutions
 
-Using F# to solve problems from [Advent of Code](http://adventofcode.com). 
+Using F# to solve problems from [Advent of Code](http://adventofcode.com).
+
+Read about my solutions on my blog:
+
+ - [Day 1](http://markheath.net/post/aoc-2016-day1)
+ - [Day 2](http://markheath.net/post/aoc-2016-day2)
+ - [Day 3](http://markheath.net/post/aoc-2016-day3) 
+
+Be inspired by [other people's solutions](https://www.reddit.com/r/adventofcode/)
