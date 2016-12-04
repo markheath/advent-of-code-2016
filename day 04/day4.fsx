50f4041d004d7d26774814396800a2a2b876b71d
Mark Heath <mark.heath@gmail.com>
2016-12-04 09:20:22 +0000

    code cleanup

--- day 04/day4.fsx
+++ day 04/day4.fsx
@@ -21,27 +21,11 @@ let isRealRoom room = calcChecksum room.name = room.checksum
 
 let input = System.IO.File.ReadAllLines (__SOURCE_DIRECTORY__ + "\\input.txt")
 
-let testInput = [| 
-    "aaaaa-bbb-z-y-x-123[abxyz]"; 
-    "a-b-c-d-e-f-g-h-987[abcde]";
-    "not-a-real-room-404[oarel]";
-    "totally-real-room-200[decoy]"
-     |]
-
-testInput |> Array.map parseRoom |> Array.filter isRealRoom |> Seq.sumBy (fun r -> r.sectorId) |> printfn "Test: %d" // 1514
 input |> Array.map parseRoom |> Array.filter isRealRoom |> Seq.sumBy (fun r -> r.sectorId) |> printfn "Part a: %d" // 158835
 
-let shiftBy (n:int) (c:char) =
-    char (int 'a' + (int c - int 'a' + n) % 26)
-
-let decryptName n name =
-    name |> Seq.map (function | '-' -> ' ' | a -> shiftBy n a) |> System.String.Concat
-
+let shiftBy (n:int) (c:char) = char (int 'a' + (int c - int 'a' + n) % 26)
+let decryptName n = Seq.map (function | '-' -> ' ' | a -> shiftBy n a) >> System.String.Concat
 let decryptRoom r = decryptName r.sectorId r.name
-
-decryptName 343 "qzmt-zixmtkozy-ivhz" |> printfn "Test: %s"
-
 let isStorageRoom r = (decryptRoom r).StartsWith("northpole object storage")
-
 let storageRoom = input |> Array.map parseRoom |> Array.filter isRealRoom |> Array.find isStorageRoom
 printfn "Part b: %d" storageRoom.sectorId // 993 
