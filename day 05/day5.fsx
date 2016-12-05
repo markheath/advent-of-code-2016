ac9af92d151e630236215b879730945f1e59a748
Mark Heath <mark.heath@gmail.com>
2016-12-05 08:15:10 +0000

    code cleanup

--- day 05/day5.fsx
+++ day 05/day5.fsx
@@ -8,16 +8,16 @@ let findHash doorId = seq {
     let inputBytes = System.Text.Encoding.ASCII.GetBytes(inputString)
     let hashBytes = md5.ComputeHash(inputBytes)
     let hashString = BitConverter.ToString(hashBytes).Replace("-","")
-    if hashString.StartsWith(prefix) then yield (n,hashString)
+    if hashString.StartsWith(prefix) then yield hashString
 }
 let input = "abbhdwsy"
 
 let findPassword doorId =
-    findHash doorId |> Seq.take 8 |> Seq.map (fun (n,h)-> h.Substring(5,1)) |> String.Concat |> (fun s -> s.ToLower())
+    findHash doorId |> Seq.take 8 |> Seq.map (fun h-> h.Substring(5,1)) |> String.Concat |> (fun s -> s.ToLower())
 
 //findPassword "abc" |> printfn "Test: %s" // 18f47a30
 
-findPassword input |> printfn "Part a: %s" 
+findPassword input |> printfn "Part a: %s" // 801b56a7
 
 let useChar (password:string) (pos, ch) =
     if pos < 8 && password.[pos] = '?' then
@@ -27,9 +27,9 @@ let useChar (password:string) (pos, ch) =
 
 let findPassword2 doorId =
     findHash doorId 
-    |> Seq.map (fun (n,h)-> int h.[5] - int '0', h.[6]) 
+    |> Seq.map (fun h -> int h.[5] - int '0', h.[6]) 
     |> Seq.scan useChar "????????"
     |> Seq.find (fun f -> f.IndexOf("?") = -1)
     |> (fun s -> s.ToLower())
 
-findPassword2 input |> printfn "Part b: %s" 
+findPassword2 input |> printfn "Part b: %s" // 424a0197
