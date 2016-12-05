c49418a6fd5b5ea25a5d854403cc6f394ab15662
Mark Heath <mark.heath@gmail.com>
2016-12-05 07:59:13 +0000

    day 5 solution

---
+++ day 05/day5.fsx
@@ -0,0 +1,35 @@
+open System;
+
+let md5 = System.Security.Cryptography.MD5.Create()
+let findHash doorId = seq {
+    let prefix = "00000"
+    for n in 0 .. 100000000 do
+    let inputString = sprintf "%s%d" doorId n
+    let inputBytes = System.Text.Encoding.ASCII.GetBytes(inputString)
+    let hashBytes = md5.ComputeHash(inputBytes)
+    let hashString = BitConverter.ToString(hashBytes).Replace("-","")
+    if hashString.StartsWith(prefix) then yield (n,hashString)
+}
+let input = "abbhdwsy"
+
+let findPassword doorId =
+    findHash doorId |> Seq.take 8 |> Seq.map (fun (n,h)-> h.Substring(5,1)) |> String.Concat |> (fun s -> s.ToLower())
+
+//findPassword "abc" |> printfn "Test: %s" // 18f47a30
+
+findPassword input |> printfn "Part a: %s" 
+
+let useChar (password:string) (pos, ch) =
+    if pos < 8 && password.[pos] = '?' then
+        sprintf "%s%c%s" (password.Substring(0,pos)) ch (password.Substring(pos+1))
+    else
+        password
+
+let findPassword2 doorId =
+    findHash doorId 
+    |> Seq.map (fun (n,h)-> int h.[5] - int '0', h.[6]) 
+    |> Seq.scan useChar "????????"
+    |> Seq.find (fun f -> f.IndexOf("?") = -1)
+    |> (fun s -> s.ToLower())
+
+findPassword2 input |> printfn "Part b: %s" 
--- readme.md
+++ readme.md
@@ -7,5 +7,6 @@ Read about my solutions on my blog:
  - [Day 1](http://markheath.net/post/aoc-2016-day1)
  - [Day 2](http://markheath.net/post/aoc-2016-day2)
  - [Day 3](http://markheath.net/post/aoc-2016-day3) 
-
+ - [Day 4](http://markheath.net/post/aoc-2016-day4)
+  
 Be inspired by [other people's solutions](https://www.reddit.com/r/adventofcode/)
