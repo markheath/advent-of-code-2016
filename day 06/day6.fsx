61866da50842eba76f42e82e392a3449ad11ad4d
Mark Heath <mark.heath@gmail.com>
2016-12-06 07:37:22 +0000

    refactored

--- day 06/day6.fsx
+++ day 06/day6.fsx
@@ -4,10 +4,6 @@ let decodePos (messages:seq<string>) selector n =
 let decodeMessages (messages:string[]) selector =
     [|0..messages.[0].Length-1|] |> Array.map (decodePos messages selector) |> System.String 
 
-let testInput = [| "eedadn"; "drvtee"; "eandsr"; "raavrd"; "atevrs"; "tsrnev"; "sdttsa"; "rasrtv"; "nssdts"; "ntnada"; "svetve"; "tesnvt"; "vntsnd"; "vrdear"; "dvrsen"; "enarar" |]
-decodeMessages testInput Seq.minBy |> printfn "Test: %s" // advent
-decodeMessages testInput Seq.maxBy |> printfn "Test: %s" // easter
-
 let input = System.IO.File.ReadAllLines (__SOURCE_DIRECTORY__ + "\\input.txt")
 decodeMessages input Seq.maxBy |> printfn "Part a: %s" // kjxfwkdh
 decodeMessages input Seq.minBy |> printfn "Part b: %s" // xrwcsnps
