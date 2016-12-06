a53f20d2084ea839fbd9c207e730b7f18ba7c996
Mark Heath <mark.heath@gmail.com>
2016-12-06 07:21:27 +0000

    day 6 solved

--- day 06/day6.fsx
+++ day 06/day6.fsx
@@ -17,7 +17,18 @@ let testInput = [|
                 "enarar"
                 |]
 
-let mostFrequentAtPos n =
-    testInput |> Seq.map (fun msg -> msg.[n]) |> Seq.countBy id |> Seq.sortByDescending (fun (a,b) -> (b,a))
+let mostFrequentAtPos (messages:seq<string>) n =
+    messages |> Seq.map (fun msg -> msg.[n]) |> Seq.countBy id |> Seq.maxBy snd |> fst
 
-mostFrequentAtPos 0
+
+
+[|0..5|] |> Array.map (mostFrequentAtPos testInput) |> System.String |> printfn "Test: %s"
+//mostFrequentAtPos 0
+let input = System.IO.File.ReadAllLines (__SOURCE_DIRECTORY__ + "\\input.txt")
+
+[|0..7|] |> Array.map (mostFrequentAtPos input) |> System.String |> printfn "Part a: %s" // kjxfwkdh
+
+let leastFrequentAtPos (messages:seq<string>) n =
+    messages |> Seq.map (fun msg -> msg.[n]) |> Seq.countBy id |> Seq.minBy snd |> fst
+[|0..5|] |> Array.map (leastFrequentAtPos testInput) |> System.String |> printfn "Test: %s"
+[|0..7|] |> Array.map (leastFrequentAtPos input) |> System.String |> printfn "Part b: %s" // xrwcsnps
