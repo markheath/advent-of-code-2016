6124d35013beae8ce2d60637d47bd471f52a2422
Mark Heath <mark.heath@gmail.com>
2016-12-06 07:33:21 +0000

    refactored

--- day 06/day6.fsx
+++ day 06/day6.fsx
@@ -1,34 +1,13 @@
-let testInput = [| 
-                "eedadn"
-                "drvtee"
-                "eandsr"
-                "raavrd"
-                "atevrs"
-                "tsrnev"
-                "sdttsa"
-                "rasrtv"
-                "nssdts"
-                "ntnada"
-                "svetve"
-                "tesnvt"
-                "vntsnd"
-                "vrdear"
-                "dvrsen"
-                "enarar"
-                |]
 
-let mostFrequentAtPos (messages:seq<string>) n =
-    messages |> Seq.map (fun msg -> msg.[n]) |> Seq.countBy id |> Seq.maxBy snd |> fst
+let decodePos (messages:seq<string>) selector n =
+    messages |> Seq.map (fun msg -> msg.[n]) |> Seq.countBy id |> selector snd |> fst
+let decodeMessages (messages:string[]) selector =
+    [|0..messages.[0].Length-1|] |> Array.map (decodePos messages selector) |> System.String 
 
+let testInput = [| "eedadn"; "drvtee"; "eandsr"; "raavrd"; "atevrs"; "tsrnev"; "sdttsa"; "rasrtv"; "nssdts"; "ntnada"; "svetve"; "tesnvt"; "vntsnd"; "vrdear"; "dvrsen"; "enarar" |]
+decodeMessages testInput Seq.minBy |> printfn "Test: %s" // advent
+decodeMessages testInput Seq.maxBy |> printfn "Test: %s" // easter
 
-
-[|0..5|] |> Array.map (mostFrequentAtPos testInput) |> System.String |> printfn "Test: %s"
-//mostFrequentAtPos 0
 let input = System.IO.File.ReadAllLines (__SOURCE_DIRECTORY__ + "\\input.txt")
-
-[|0..7|] |> Array.map (mostFrequentAtPos input) |> System.String |> printfn "Part a: %s" // kjxfwkdh
-
-let leastFrequentAtPos (messages:seq<string>) n =
-    messages |> Seq.map (fun msg -> msg.[n]) |> Seq.countBy id |> Seq.minBy snd |> fst
-[|0..5|] |> Array.map (leastFrequentAtPos testInput) |> System.String |> printfn "Test: %s"
-[|0..7|] |> Array.map (leastFrequentAtPos input) |> System.String |> printfn "Part b: %s" // xrwcsnps
+decodeMessages input Seq.maxBy |> printfn "Part a: %s" // kjxfwkdh
+decodeMessages input Seq.minBy |> printfn "Part b: %s" // xrwcsnps
