409ea263b86a8976fcaa2d7f5f626326ae98f6de
Mark Heath <mark.heath@gmail.com>
2016-12-08 19:18:19 +0000

    day 8 solution

--- day 08/day8.fsx
+++ day 08/day8.fsx
@@ -1,49 +1,113 @@
-open System.Text.RegularExpressions;
-let input = System.IO.File.ReadAllLines (__SOURCE_DIRECTORY__ + "\\input.txt")
+open System.Text.RegularExpressions
 
-type Instruction = Rect of int * int | RotateRow of int * int | RotateCol of int * int 
+type Instruction = Rect of int*int | RotateRow of int*int | RotateCol of int*int
+
 let (|Matches|) pattern =
   let rgx = Regex (pattern, RegexOptions.Compiled)
-  fun input -> [for m in rgx.Matches input -> [for g in m.Groups -> g.Value]]
+  fun input -> [for m in rgx.Matches input do for g in m.Groups -> g.Value]
 
 let (|Int|_|) str = match System.Int32.TryParse str with true, value -> Some value | _ -> None
 
 let parseInstruction = function 
-    | Matches "rect (\d+)x(\d+)" [[_; Int width; Int height ]] -> Rect (width, height)
-    | Matches "rotate row y=(\d+) by (\d+)" [[_; Int r; Int n ]] -> RotateRow (r, n)
-    | Matches "rotate column x=(\d+) by (\d+)" [[_; Int r; Int n ]] -> RotateCol (r, n)
-    | u -> failwith ("oops " + u) 
+    | Matches @"rect (\d+)x(\d+)" [_; Int width; Int height] -> Rect(width,height)
+    | Matches @"rotate row y\=(\d+) by (\d+)" [_; Int row; Int steps] -> RotateRow(row,steps)
+    | Matches @"rotate column x\=(\d+) by (\d+)" [_; Int row; Int steps] -> RotateCol(row,steps)
+    | x -> failwith ("parseError " + x)
+  
+let instructions = System.IO.File.ReadAllLines (__SOURCE_DIRECTORY__ + "\\input.txt") |> Array.map parseInstruction
+
+let rect (width,height) (state:int[,]) = 
+    let out = Array2D.copy state
+    for w in 0..width-1 do
+        for h in 0..height-1 do
+            out.[h,w] <- 1
+    out
+
+let rotateRow (row,steps) (state:int[,]) = 
+    let out = Array2D.copy state
+    let rowLength = Array2D.length2 state
+    for w in 0..rowLength-1 do
+        out.[row,(w+steps)%rowLength] <- state.[row,w]
+    out
+  
+let rotateCol (col,steps) (state:int[,]) = 
+    let out = Array2D.copy state
+    let colLength = Array2D.length1 state
+    for w in 0..colLength-1 do
+        out.[(w+steps)%colLength,col] <- state.[w,col]
+    out 
+
+let applyInst (state:int[,]) = function
+    | Rect(a,b) -> rect (a,b) state
+    | RotateRow(a,b) -> rotateRow (a,b) state
+    | RotateCol(a,b) -> rotateCol (a,b) state
+
+[| Rect (3,2) ; RotateCol (1,1) ; RotateRow (0,4); RotateCol (1,1)  |]
+    |> Array.fold applyInst (Array2D.create 3 7 0)
+    |> Seq.cast<int>
+    |> Seq.sum
+    |> printfn "Test: %d"
+
+instructions |> Array.fold applyInst (Array2D.create 6 50 0) |> Seq.cast<int> |> Seq.sum |> printfn "Part a: %d" // 116
+
+let endState = instructions |> Array.fold applyInst (Array2D.create 6 50 0) // UPOJFLBCEZ
+
+(* TO SAVE AS BITMAP
+open System.Windows.Forms
+open System.Drawing
+
+let drawState (state:int[,]) =
+    let width =Array2D.length2 state
+    let height = Array2D.length1 state
+    let bm = new Bitmap (width, height)
+    for x in {0..width-1} do
+        for y in {0..height-1} do
+            bm.SetPixel(x,y, if state.[y,x] = 1 then Color.Black else Color.White)
+    bm
+
+let image  = drawState endState 
+//image.Save(imagePath)
+*)
+
+#if INTERACTIVE
+#r "PresentationCore.dll"
+#r "PresentationFramework.dll"
+#r "System.Xaml.dll"
+#r "UIAutomationTypes.dll"
+#r "WindowsBase.dll"
+#endif
+
+open System.Windows
+open System.Windows.Media
+open System.Windows.Media.Imaging
+open System.Windows.Shapes
+open System.Windows.Controls
 
 
-let rect (width,height) (state:int[,]) =
-    let changed = Array2D.copy state
-    for x in 0..width-1 do
-        for y in 0..height-1 do
-            changed.[y,x] <- 1
-    changed
 
-let rotateRow (row,n) (state:int[,]) =
-    let changed = Array2D.copy state
-    let rowWidth = Array2D.length2 state
-    for x in 0..rowWidth-1 do
-        changed.[row, ((x+n)%rowWidth)] <- state.[row,x]
-    changed
+let drawState (state:int[,]) scale =
+   
+    let width = Array2D.length2 state
+    let height = Array2D.length1 state
+    let canvas = new Canvas(Background=Brushes.DarkGreen,
+                            RenderTransform=ScaleTransform(ScaleX = scale, ScaleY = scale),
+                            Width = (float width),
+                            Height = (float height),
+                            HorizontalAlignment = HorizontalAlignment.Left,
+                            VerticalAlignment = VerticalAlignment.Top
+                            )
+    for x in {0..width-1} do
+        for y in {0..height-1} do
+            if state.[y,x] = 1 then 
+                let p = Rectangle(Width=1.0,Height=1.0,Fill=Brushes.Lime)
+                canvas.Children.Add(p) |> ignore
+                Canvas.SetLeft(p, float x)
+                Canvas.SetTop(p, float y)
+    printfn "%f %f" canvas.Width canvas.Height    
+    canvas
 
-let rotateCol (col,n) (state:int[,]) =
-    let changed = Array2D.copy state
-    let colHeight = Array2D.length1 state
-    for y in 0..colHeight-1 do
-        changed.[((y+n)%colHeight),col] <- state.[y,col]
-    changed
-
-let testState = Array2D.create 3 7 0
-
-rect (3,2) testState |> rotateCol (1,1) |> rotateRow (0,4) |> rotateCol (1,1)
-let startState = Array2D.create 6 50 0
-
-let apply = function | Rect (a,b) -> rect (a,b) | RotateCol (a,b) -> rotateCol (a,b) | RotateRow (a,b) -> rotateRow (a,b)
-
-input 
-    |> Array.map parseInstruction 
-    |> Array.fold apply startState 
-
+let scaleFactor = 10.0
+let canvas = drawState endState scaleFactor
+let window = Window(Title ="Advent of Code", Content=canvas, 
+                    Width = 20.0 +  canvas.Width * scaleFactor, Height = 40.0 + canvas.Height * scaleFactor)
+window.ShowDialog()
