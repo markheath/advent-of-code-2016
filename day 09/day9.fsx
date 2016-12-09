eb95c781a02058eb84b957755ac820eaaa132286
Mark Heath <mark.heath@gmail.com>
2016-12-09 08:09:39 +0000

    part b solved

--- day 09/day9.fsx
+++ day 09/day9.fsx
@@ -35,3 +35,28 @@ decompress "(6x1)(1x3)A"
 decompress "X(8x2)(3x3)ABCY"
 
 (decompress input).Length |> printfn "Part a: %d"
+
+
+type ExpandState2 = { parsePos: int; decodedLen: int64 }
+
+let rec decompressLen (input:string) =
+    let rec expand state =
+        let m = Regex.Match(input.Substring(state.parsePos), @"^\((\d+)x(\d+)\)")
+        if state.parsePos >= input.Length then 
+            state.decodedLen
+        else
+            if m.Success then
+                let chars = int m.Groups.[1].Value
+                let repeats = int m.Groups.[2].Value
+                let repeat = input.Substring(state.parsePos + m.Index + m.Length, chars)
+                let expandedLen = (decompressLen repeat) * int64 repeats 
+                expand { parsePos = state.parsePos+m.Length+chars;
+                         decodedLen = state.decodedLen + expandedLen}
+            else
+                expand { parsePos = state.parsePos+1; decodedLen = state.decodedLen+1L }
+    
+    expand { parsePos = 0; decodedLen = 0L}
+
+decompressLen "(27x12)(20x12)(13x14)(7x10)(1x12)A" // 241920
+decompressLen "(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN" // 445
+decompressLen input
