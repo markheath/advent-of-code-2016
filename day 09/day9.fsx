3883d597ee6c5c156f34621461c5b108ddcaa481
Mark Heath <mark.heath@gmail.com>
2016-12-09 12:17:10 +0000

    simplified decompressLen

--- day 09/day9.fsx
+++ day 09/day9.fsx
@@ -35,22 +35,19 @@ type ExpandState2 = { parsePos: int; decodedLen: int64 }
 
 let rec decompressLen (input:string) =
     let rec expand state =
-        let m = Regex.Match(input.Substring(state.parsePos), @"^\((\d+)x(\d+)\)")
-        if state.parsePos >= input.Length then 
-            state.decodedLen
+        let m = Regex.Match(input.Substring(state.parsePos), @"\((\d+)x(\d+)\)")
+        if m.Success then
+            let chars = int m.Groups.[1].Value
+            let repeats = int m.Groups.[2].Value
+            let repeat = input.Substring(state.parsePos + m.Index + m.Length, chars)
+            let expandedLen = (decompressLen repeat) * int64 repeats 
+            expand { parsePos = state.parsePos + m.Index + m.Length + chars;
+                        decodedLen = state.decodedLen + int64 m.Index + expandedLen}
         else
-            if m.Success then
-                let chars = int m.Groups.[1].Value
-                let repeats = int m.Groups.[2].Value
-                let repeat = input.Substring(state.parsePos + m.Index + m.Length, chars)
-                let expandedLen = (decompressLen repeat) * int64 repeats 
-                expand { parsePos = state.parsePos+m.Length+chars;
-                         decodedLen = state.decodedLen + expandedLen}
-            else
-                expand { parsePos = state.parsePos+1; decodedLen = state.decodedLen+1L }
+            state.decodedLen + int64(input.Length-state.parsePos)
     
     expand { parsePos = 0; decodedLen = 0L}
 
-decompressLen "(27x12)(20x12)(13x14)(7x10)(1x12)A" // 241920
-decompressLen "(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN" // 445
+decompressLen "(27x12)(20x12)(13x14)(7x10)(1x12)A" |> printfn "Test: %d" // 241920
+decompressLen "(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN" |> printfn "Test: %d"  // 445
 decompressLen input |> printfn "Part b: %d" // 10774309173
