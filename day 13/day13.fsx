77bd5979f57bfee586629b503a4c063297562d55
Mark Heath <mark.heath@gmail.com>
2016-12-13 08:08:54 +0000

    part b solved

--- day 13/day13.fsx
+++ day 13/day13.fsx
@@ -6,9 +6,11 @@ let countBits number =
             countBits' (count + if set then 1 else 0)  (number >>> 1)
     countBits' 0 number
 
-let isWall favourite (x,y) = 
-    let n = x*x + 3*x + 2*x*y + y + y*y
-    (countBits (n + favourite)) % 2 = 1
+let isWall favourite (x,y) =
+    if x < 0 || y < 0 then true
+    else
+        let n = x*x + 3*x + 2*x*y + y + y*y
+        (countBits (n + favourite)) % 2 = 1
 
 
 //countBits 5
@@ -16,17 +18,17 @@ let isWall favourite (x,y) =
 //isWall 1350 400 400
 
 open System.Collections.Generic
-let search (start:int*int) (target:int*int) (isWall:int*int->bool) =
+let search (start:int*int) isTarget (isWall:int*int->bool) =
     let points = new Queue<int*int>()
     let distances = new Dictionary<int*int,int>()
     let rec search'() =
         if points.Count = 0 then
-            failwith "no path found"
+            -1, distances
         else
             let (x,y) = points.Dequeue()
             let steps = distances.[(x,y)]
-            if (x,y) = target then
-                steps
+            if isTarget (x,y) steps then
+                steps, distances
             else
                 let newPoints = [(x+1,y);(x-1,y);(x,y+1);(x,y-1)]
                 for newPoint in newPoints do
@@ -40,10 +42,18 @@ let search (start:int*int) (target:int*int) (isWall:int*int->bool) =
     distances.[start] <- 0
     points.Enqueue(start)
     search'()
-                    
 
-search (1,1) (7,4) (isWall 10) // 11
-search (1,1) (31,39) (isWall 1350) // part a: 92
+
+
+search (1,1) (fun a b -> a = (7,4)) (isWall 10) |> fst // 11
+search (1,1) (fun a b -> a = (31,39)) (isWall 1350) |> fst // part a: 92
+
+search (1,1) (fun a b -> b = 51) (isWall 1350) 
+    |> snd 
+    |> (fun a -> a.Values)
+    |> Seq.filter (fun a -> a <= 50) 
+    |> Seq.length
+   // part b: 124
 
 let input = 1350
 let start = (1,1)
