b22516336f3e91e0acaa7fc961b0859027d39a31
Mark Heath <mark.heath@gmail.com>
2016-12-17 08:48:02 +0000

    part b solved

--- day 17/day17.fsx
+++ day 17/day17.fsx
@@ -10,18 +10,18 @@ let hash (s:string) =
 let bfs (isSolution:'a->bool) (getChildren:'a->seq<'a>) (start:'a) =
     let q = new Queue<'a>()
     q.Enqueue(start)
-    let rec search() =
-        if q.Count = 0 then
-            failwith "no solution"
-        else
+    let rec search() = seq {
+        if q.Count > 0 then
             let s = q.Dequeue()
-            if isSolution s then s
+            if isSolution s then 
+                yield s
             else
                 for c in getChildren s do
                     q.Enqueue(c)
-                search()
+            yield! search()
+    }
     search()
-        
+
 type State = { pos: int*int; path:string}
 
 let isSolution state = state.pos = (3,3)
@@ -40,11 +40,10 @@ let getValidMoves passcode state = seq {
     if isOpen h.[3] && x < 3 then
         yield { pos = (x+1,y); path = state.path + "R" }
 }
-
 let startState = { pos=(0,0); path="" }
 let solve passcode =    
-    let s = bfs isSolution (getValidMoves passcode) startState 
-    s.path
+    let s = bfs isSolution (getValidMoves passcode) startState |> Seq.head
+    s.path 
 
 getValidMoves "hijkl" startState |> Seq.toArray |> printfn "Test %A" // down only
 getValidMoves "hijkl" { pos=(0,1); path="D"} |> Seq.toArray |> printfn "Test %A" // up or right
@@ -54,9 +53,20 @@ let assertEquals actual expected =
     if actual = expected then
         printfn "PASS %A" actual
     else 
-        printfn "FAIL: got %A expected %A" expected actual
+        printfn "FAIL: got %A expected %A" actual expected
 
 assertEquals (solve "ihgpwlah") "DDRRRD"
 assertEquals (solve "kglvqrro") "DDUDRLRRUDRD"
 assertEquals (solve "ulqzkmiv") "DRURDRUDDLLDLUURRDULRLDUUDDDRR"
 solve "pxxbnzuo" |> printfn "Part a: %s" // not 10, RDULRDDRRD
+
+let solveB passcode =    
+    bfs isSolution (getValidMoves passcode) startState 
+        |> Seq.map (fun s -> s.path.Length)
+        |> Seq.max 
+
+assertEquals (solveB "ihgpwlah") 370
+assertEquals (solveB "kglvqrro") 492
+assertEquals (solveB "ulqzkmiv") 830
+
+bfs isSolution (getValidMoves "ihgpwlah") startState 
