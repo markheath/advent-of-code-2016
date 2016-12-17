6d4147f445cbe44efc8877e2c9581a52d891c6b2
Mark Heath <mark.heath@gmail.com>
2016-12-17 08:23:27 +0000

    day 17 test cases pass, part a fails

---
+++ day 17/day17.fsx
@@ -0,0 +1,60 @@
+open System.Text
+open System
+open System.Collections.Generic
+let md5 = System.Security.Cryptography.MD5.Create()
+let hash (s:string) =
+    Encoding.Default.GetBytes(s)
+    |> md5.ComputeHash
+    |> (fun h -> BitConverter.ToString(h).ToLower().Replace("-",""))
+
+let bfs (isSolution:'a->bool) (getChildren:'a->seq<'a>) (start:'a) =
+    let q = new Queue<'a>()
+    q.Enqueue(start)
+    let rec search() =
+        if q.Count = 0 then
+            failwith "no solution"
+        else
+            let s = q.Dequeue()
+            if isSolution s then s
+            else
+                for c in getChildren s do
+                    q.Enqueue(c)
+                search()
+    search()
+        
+type State = { pos: int*int; path:string}
+
+let isSolution state = state.pos = (3,3)
+
+let isOpen (c:char) = c >= 'b' && c <= 'f'
+
+let getValidMoves passcode state = seq {
+    let (x,y) = state.pos
+    let h = hash (passcode + state.path)
+    if isOpen h.[0] && y > 0 then
+        yield { pos = (x,y-1); path = state.path + "U" }
+    if isOpen h.[1] && y < 3 then
+        yield { pos = (x,y+1); path = state.path + "D" }
+    if isOpen h.[2] && x > 0 then
+        yield { pos = (x-1,y); path = state.path + "L" }
+    if isOpen h.[3] && x < 3 then
+        yield { pos = (x+1,y); path = state.path + "R" }
+}
+
+let startState = { pos=(0,0); path="" }
+let solve passcode =    
+    let s = bfs isSolution (getValidMoves passcode) startState 
+    s.path
+
+getValidMoves "hijkl" startState |> Seq.toArray |> printfn "Test %A" // down only
+
+let assertEquals actual expected =
+    if actual = expected then
+        printfn "PASS %A" actual
+    else 
+        printfn "FAIL: got %A expected %A" expected actual
+
+assertEquals (solve "ihgpwlah") "DDRRRD"
+assertEquals (solve "kglvqrro") "DDUDRLRRUDRD"
+assertEquals (solve "ulqzkmiv") "DRURDRUDDLLDLUURRDULRLDUUDDDRR"
+(solve "pxxbnzuo").Length |> printfn "Part a: %d" // not 10
