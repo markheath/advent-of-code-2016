f3159753463fc22922fc104449d528f49c99b72d
Mark Heath <mark.heath@gmail.com>
2016-12-20 15:18:08 +0000

    improved final range calculation

--- day 20/day20.fsx
+++ day 20/day20.fsx
@@ -7,9 +7,10 @@ let ranges = System.IO.File.ReadAllLines (__SOURCE_DIRECTORY__ + "\\input.txt")
 
 
 let getAllowed blockedRanges =
+    let endRange = 4294967295L
     let rec mergeRanges n ranges = seq {
         match ranges with
-        | [] -> yield (n,4294967295L)
+        | [] -> if n <= endRange then yield (n,endRange)
         | (lo,hi)::tail ->
             //printfn "%d %d,%d" n lo hi
             if n < lo then yield (n,lo-1L)
