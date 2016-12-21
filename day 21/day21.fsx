2cb72adabf0cceac657f0e08941b0fb619273d7c
Mark Heath <mark.heath@gmail.com>
2016-12-21 12:53:20 +0000

    removing redundant code

--- day 21/day21.fsx
+++ day 21/day21.fsx
@@ -11,57 +11,24 @@ let swap x y (input:char[]) =
                             elif n = y then input.[x]
                             else c) input
 
-swap 1 2 ("hello".ToCharArray())
-
 let swapLetter x y =
     Array.map (fun c -> if c = x then y elif c = y then x else c)
 
-swapLetter 'e' 'l' ("hello".ToCharArray())
-
 let rotateRight steps (input:char[]) =
-    //printfn "rotate right %d %s" steps (System.String(input))
     let s = steps % input.Length
     Array.append input.[input.Length-s..input.Length-1] input.[0..input.Length-s-1] 
 
-rotateRight 2 ("hello".ToCharArray())
-rotateRight 6 ("hello".ToCharArray())
-
 let rotateLeft steps (input:char[]) =
     Array.append input.[steps..input.Length-1] input.[0..steps-1] 
-
-rotateLeft 2 ("hello".ToCharArray())
-rotateLeft 4 ("hello".ToCharArray())
 
 let rotatePos c (input:char[]) =
     let n = Array.findIndex ((=) c) input
     let rot = n + (if n >= 4 then 2 else 1)
     rotateRight rot input
 
-rotatePos 'a' ("abcde".ToCharArray()) // should rotate by 1
-rotatePos 'b' ("abcde".ToCharArray()) // should rotate by 2
-rotatePos 'c' ("abcde".ToCharArray()) // should rotate by 3
-rotatePos 'd' ("abcde".ToCharArray()) // should rotate by 4
-rotatePos 'e' ("abcde".ToCharArray()) // should rotate by 6
-
-rotateRight 2 ("abcde".ToCharArray()) 
-
 let reverse x y (input:char[]) =
     Array.mapi (fun n c -> if n < x || n > y then input.[n]
                            else input.[x + y - n]) input
-
-reverse 1 3 ("habco".ToCharArray())
-
-let moveX x y (input:char[]) =
-    [|0..input.Length-1|]
-    |> Array.map (fun n ->
-    if (n < x && n < y) || (n > x && n > y) then
-            input.[n]
-        else if n = y then
-            input.[x]
-        else if x < y then
-            input.[n+1]
-        else
-            input.[n-1])
 
 let move x y (input:char[]) =
     input |> Array.mapi (fun n c ->
@@ -73,15 +40,6 @@ let move x y (input:char[]) =
             input.[n+1]
         else
             input.[n-1])
-
-
-move 1 3 ("abcde".ToCharArray())
-move 3 1 ("abcde".ToCharArray())
-move 3 0 ("abcde".ToCharArray())
-move 0 3 ("abcde".ToCharArray())
-move 0 4 ("abcde".ToCharArray())
-move 4 0 ("abcde".ToCharArray())
-move 4 1 ("abcde".ToCharArray())
 
 let apply = function
             | SwapPos (x,y) -> swap x y 
@@ -107,21 +65,6 @@ let parseInstruction (inst:string) =
 let scramble (input:string) instructions =
     instructions |> Seq.map parseInstruction |> Seq.fold (fun s i -> apply i s) (input.ToCharArray()) |> System.String
 
-let testInstructions = [ 
-                        "swap position 4 with position 0"
-                        "swap letter d with letter b"
-                        "reverse positions 0 through 4"
-                        "rotate left 1 step"
-                        "move position 1 to position 4"
-                        "move position 3 to position 0"
-                        "rotate based on position of letter b"
-                        "rotate based on position of letter d" 
-                        ]
-
-testInstructions
-|> scramble "abcde"
-|> printfn "Test: %s" // decab
-
 System.IO.File.ReadAllLines (__SOURCE_DIRECTORY__ + "\\input.txt") 
 |> scramble "abcdefgh"
 |> printfn "Part a: %s" // gbhafcde
@@ -144,24 +87,8 @@ let undo = function
             | RotatePos c -> undoRotate c
             | Reverse (x,y) -> reverse x y 
             | Move (x,y) -> move y x
-
-let roundTrip inst (s:string) =
-    let applied = apply inst (s.ToCharArray())
-    undo inst applied |> System.String
-
-roundTrip (SwapPos (1,4)) "abcde" |> printfn "%s"
-roundTrip (SwapLetter ('a','c')) "abcde" |> printfn "%s"
-roundTrip (RotateRight 4) "abcde" |> printfn "%s"
-roundTrip (RotateLeft 4) "abcde" |> printfn "%s"
-roundTrip (Move (1,4)) "abcde" |> printfn "%s"
-roundTrip (Reverse (2,4)) "abcde" |> printfn "%s"
-
 let unscramble (input:string) instructions =
     instructions |> Seq.map parseInstruction |> Seq.rev |> Seq.fold (fun s i -> undo i s) (input.ToCharArray()) |> System.String
-
-testInstructions
-|> unscramble "decab"
-|> printfn "Test: %s"
 
 System.IO.File.ReadAllLines (__SOURCE_DIRECTORY__ + "\\input.txt") 
 |> unscramble "fbgdceah"
