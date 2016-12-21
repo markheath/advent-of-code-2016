670911b0acd95a3b9fdf85ae01c61cfc0227d558
Mark Heath <mark.heath@gmail.com>
2016-12-21 13:20:48 +0000

    code simplification

--- day 21/day21.fsx
+++ day 21/day21.fsx
@@ -6,40 +6,30 @@ type Instruction = SwapPos of int * int
                    | Reverse of int * int
                    | Move of int * int
 
-let swap x y (input:char[]) =
-    Array.mapi (fun n c -> if n = x then input.[y] 
-                            elif n = y then input.[x]
-                            else c) input
+let swap x y (a:_[]) = Array.mapi (fun n c -> if n = x then a.[y] elif n = y then a.[x] else c) a
 
-let swapLetter x y =
-    Array.map (fun c -> if c = x then y elif c = y then x else c)
+let swapLetter x y = Array.map (fun c -> if c = x then y elif c = y then x else c)
 
-let rotateRight steps (input:char[]) =
-    let s = steps % input.Length
-    Array.append input.[input.Length-s..input.Length-1] input.[0..input.Length-s-1] 
+let rotateLeft n (a:_[]) = [|for i in 0..a.Length-1 -> a.[(n+i)%a.Length]|]
+let rotateRight n (a:_[]) = rotateLeft (a.Length-(n%a.Length)) a
 
-let rotateLeft steps (input:char[]) =
-    Array.append input.[steps..input.Length-1] input.[0..steps-1] 
+let rotatePos c (a:_[]) =
+    let n = Array.findIndex ((=) c) a
+    rotateRight (n + if n >= 4 then 2 else 1) a
 
-let rotatePos c (input:char[]) =
-    let n = Array.findIndex ((=) c) input
-    let rot = n + (if n >= 4 then 2 else 1)
-    rotateRight rot input
+let reverse x y (a:_[]) =
+    Array.mapi (fun n c -> if n < x || n > y then a.[n] else a.[x + y - n]) a
 
-let reverse x y (input:char[]) =
-    Array.mapi (fun n c -> if n < x || n > y then input.[n]
-                           else input.[x + y - n]) input
-
-let move x y (input:char[]) =
-    input |> Array.mapi (fun n c ->
+let move x y (a:_[]) =
+    a |> Array.mapi (fun n c ->
         if (n < x && n < y) || (n > x && n > y) then
-            input.[n]
+            a.[n]
         else if n = y then
-            input.[x]
+            a.[x]
         else if x < y then
-            input.[n+1]
+            a.[n+1]
         else
-            input.[n-1])
+            a.[n-1])
 
 let apply = function
             | SwapPos (x,y) -> swap x y 
