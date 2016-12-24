ae411ebce616d8c12091bda8a3cb0ee9ea65b2d6
Mark Heath <mark.heath@gmail.com>
2016-12-24 09:11:13 +0000

    day 24 shortest path finder

---
+++ day 24/day24.fsx
@@ -0,0 +1,47 @@
+// n.b. day 13 has bfs maze solver, day 17 has generalized bfs
+// input has 8 locations (0-7)
+open System.Collections.Generic
+
+let bfs (isSolution:'a->bool) (getChildren:'a->seq<'a>) (start:'a) =
+    let q = new Queue<'a>()
+    q.Enqueue(start)
+    let rec search() = seq {
+        if q.Count > 0 then
+            let s = q.Dequeue()
+            if isSolution s then 
+                yield s
+            //else
+            for c in getChildren s do
+                q.Enqueue(c)
+            yield! search()
+    }
+    search()
+
+let shortestFrom maze start =
+    let startc = maze start
+    let dist = Dictionary<int*int,int>()
+    let getChildren ((x,y),d) = 
+        [(-1,0);(0,-1);(1,0);(0,1)]
+        |> Seq.map (fun (i,j) -> (x+i,y+j) )
+        |> Seq.filter (dist.ContainsKey >> not)
+        |> Seq.filter (fun p -> maze p <> '#')
+        |> Seq.map (fun p -> dist.[p] <- d+1; (p,d+1))
+    let isSolution (pos,d) =
+        let c = maze pos
+        c >= '0' && c < '9' && c <> startc
+    bfs isSolution getChildren (start,0) |> Seq.map (fun (p,d) -> maze p,d)
+    
+let mazeLookup (maze:string[]) (x,y) = maze.[y].[x]
+let mazeFind (maze:string[]) c = 
+    [for y in 0..maze.Length-1 do 
+        for x in 0 ..maze.[y].Length-1 do 
+            if maze.[y].[x] = c then yield (x,y)]
+            |> List.head
+
+
+let maze = System.IO.File.ReadAllLines (__SOURCE_DIRECTORY__ + "\\input.txt")
+let testMaze = System.IO.File.ReadAllLines (__SOURCE_DIRECTORY__ + "\\testinput.txt")
+
+
+shortestFrom (mazeLookup maze) (mazeFind maze '4')
+    |> Seq.iter (printfn "%A")
